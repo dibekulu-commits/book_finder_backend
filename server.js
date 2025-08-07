3345038ac210a0988edb276bb3d054d37e371aca
dibekulu-commits <adibekulu@gmail.com>
2025-08-07 16:14:00 -0400

    some modification

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { sequelize } = require('./models'); // Ensure this imports correctly
+const { sequelize } = require('./models'); 
 const bookRoutes = require('./routes/bookRoutes');
 
 app.use(cors());
